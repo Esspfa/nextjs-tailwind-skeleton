c0c13717cdb16d33d21546bdcc9cf16fd4854a8c
Sarmed Rizvi <sarmedrizvi@gmail.com>
2022-04-12 14:14:36 +0500

    updates

--- layout/footer.tsx
+++ layout/footer.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <div className="bg-black w-[100%] mt-auto">
+    <div className="bg-black w-full mt-auto">
       <div className="xs:container mx-auto xs:px-0 px-2 pt-24 pb-10">footer</div>
     </div>
   );
--- layout/header.tsx
+++ layout/header.tsx
@@ -1,7 +1,5 @@
 /* eslint-disable @next/next/no-img-element */
 import React, { useState } from 'react';
-import Link from 'next/link';
-import { Drawer } from './drawer';
 import styles from './layout.module.scss';
 
 interface Props {
@@ -11,29 +9,18 @@ export default function Header(props: Props) {
   const { transparent } = props;
   const [openDrawer, setOpenDrawer]: [boolean, Function] = useState(false);
   return (
-    <div className={transparent ? styles.header : 'bg-black'}>
-      <Drawer open={openDrawer} setOpen={setOpenDrawer} />
-      <div className=" w-[95%] mx-auto py-4">
+    <div className={transparent ? styles.header : 'bg-white'}>
+      <div className="w-11/12 mx-auto py-4">
         <div className="flex items-center justify-between">
           <div className="flex items-center">
             <img
               width="24px"
               height="24px"
-              className="cursor-pointer md:hidden mr-[16px]"
+              className="cursor-pointer md:hidden mr-4"
               src="/svg/icon-menu.svg"
               alt="menu"
               onClick={() => setOpenDrawer(!openDrawer)}
             />
-          </div>
-          <div className="uppercase flex items-center">
-            <div className="pr-4 sm:block hidden">for business</div>
-            <div className="flex align-center">
-              <select className={transparent ? styles.invisible : 'bg-primary'}>
-                <option>EN</option>
-              </select>
-              {/* <span className="pr-1">EN</span>{' '}
-            <Image width={'16px'} height="16px" alt="chevron" src={'/svg/icon-chevron-down.svg'} /> */}
-            </div>
           </div>
         </div>
       </div>
